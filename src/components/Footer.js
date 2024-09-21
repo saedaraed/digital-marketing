cc458e97cefd5fc154a9ee52fbc179b50c8b4e36
saedaraed <saedaraed19@gmail.com>
2024-09-21 23:58:46 +0300

    change color to test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { FaFacebookF, FaLinkedinIn, FaTwitter } from 'react-icons/fa'; // Icons
 
 const Footer = () => {
   return (
-    <footer className=" py-8 text-black dark:text-white">
+    <footer className=" py-8 text-black dark:text-green">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0">
 
         <div className="text-2xl font-bold"> <svg width="47" height="61" viewBox="0 0 47 61" fill="none" xmlns="http://www.w3.org/2000/svg">
