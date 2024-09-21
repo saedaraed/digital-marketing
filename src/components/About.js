7c626c10a15fa32c1724f3b2cdad7eed6946e5e7
saedaraed <saedaraed19@gmail.com>
2024-09-21 23:15:19 +0300

    make more details

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,29 @@
 import '../index.css';
 import profileImage from '../assets/WhatsApp Image 2024-09-07 at 18.22.11.jpeg';
 import { useTranslation } from 'react-i18next';
+import { useInView } from 'react-intersection-observer';
 
-const About = () => {
+const About = ({ id }) => {
   const { t, i18n } = useTranslation();
  
   const isArabic = i18n.language === 'ar';
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+    triggerOnce: true,
+  });
   return (
-    <section className={`bg-custom-dark flex justify-center items-center py-16 px-8 font-hind ${isArabic ? 'arabic-font' : ''}`} dir={isArabic ? 'rtl' : 'ltr'}>
-      <div className="bg-white max-w-6xl mx-auto flex flex-col md:flex-row items-center h-auto md:h-[704px]">
+    <section  id={id}
+    ref={ref} className={`bg-white dark:bg-custom-dark flex justify-center items-center py-16 px-8 font-hind opacity-0 transform translate-y-10 transition-all duration-700 ease-in-out md:opacity-100 md:translate-y-0 ${isArabic ? 'arabic-font' : ''} ${inView ? 'animate-fadeIn' : 'opacity-0'}`} dir={isArabic ? 'rtl' : 'ltr'}>
+      <div  className="bg-custom-dark dark:bg-white  max-w-6xl mx-auto flex flex-col md:flex-row items-center h-auto md:h-[704px]">
        
         <div className="px-4 sm:px-8 md:px-16 h-full w-full md:w-1/2 flex flex-col justify-center text-left py-8 md:py-0">
           <h2 className="text-pink-500 text-xl font-bold mb-7 border-b-4 border-gray-400 inline-block w-max">
           {t('aboutMe')}
           </h2>
-          <h1 className={`"text-black text-2xl sm:text-3xl md:text-4xl font-bold mb-4 leading-relaxed" ${isArabic ? 'text-right' : 'text-left'}`}>
+          <h1 className={`" text-white dark:text-black  text-2xl sm:text-3xl md:text-4xl font-bold mb-4 leading-relaxed" ${isArabic ? 'text-right' : 'text-left'}`}>
            {t('quote')}
           </h1>
-          <p className={`"text-gray-700 mb-4 text-[18px] sm:text-[20px]" ${isArabic ? 'text-right' : 'text-left'}`}>
+          <p className={`" text-gray-700 dark:text-gray-700 mb-4 text-[18px] sm:text-[20px]" ${isArabic ? 'text-right' : 'text-left'}`}>
            {t('informationAboutMe')}
           </p>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
@@ -11,10 +12,27 @@ module.exports = {
         'custom-dark': '#090629',
       },
       boxShadow: {
-        'custom': '-5px 5px 20px 5px rgba(255, 255, 255, 0.15)', 
+        'customdark': '-5px 5px 20px 5px rgba(255, 255, 255, 0.15)', 
+        'customlight': '-5px 5px 20px 5px rgba(0, 0, 0, 0.15)', 
+
       },
       fontFamily: {
         'hind': ['Hind', 'sans-serif'],
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        slideUp: {
+          '0%': { transform: 'translateY(50px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out',
+        slideUp: 'slideUp 1s ease-in-out',
+
       },
   
     },
